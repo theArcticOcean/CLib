c498f0bc3c7977847d0384ed1daad32637d41585
edemon <wei_yang1994@163.com>
2017-12-30 16:44:11 +0800

    rm http

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-This folder stores some C libraries and simple examples.
 <li> sem.c: a simple semaphore using example.
 <li> popen.c: a simple popen using example.
 <li> http_down: a http down library implemented with curl tool.
--- http_down/build.sh
+++
@@ -1,2 +0,0 @@
-#qcc -Vgcc_ntoarmv7le -Wl,--no-keep-memory  -lang-c++ -lm -Wl,--rpath=. -lcurl -L./ -o main  http_request_mgr.cpp main.cpp SignatureGenerator.cpp
-g++ -lcurl -L./ -o main http_request_mgr.cpp main.cpp SignatureGenerator.cpp
