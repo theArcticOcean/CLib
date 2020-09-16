2c8be4cb56e21dd23e9c7f6d9d3389d28e56ea32
WeiYang <wei_yang520@hotmail.com>
2020-09-16 19:54:51 +0800

    Finish Find Sperated Lists

--- VTKLearn/Explore/main.cpp
+++ VTKLearn/Explore/main.cpp
@@ -18,33 +18,66 @@
 #include <vtkColorTransferFunction.h>
 #include <vtkCellData.h>
 #include <vtkPoints.h>
+#include <vtkPlane.h>
 #include <vtkCell.h>
 #include <vtkCellArray.h>
 #include <vtkTriangle.h>
 #include <vtkLineSource.h>
 #include <vtkTriangleFilter.h>
+#include <vtkCutter.h>
+#include <QString>
 #include <vtkXMLPolyDataReader.h>
+#include <vtkProperty.h>
 
+#include "ListsFilter.h"
 #include "../tool.h"
 
 using namespace std;
 
-#define vtkSPtr vtkSmartPointer
-#define vtkSPtrNew(Var, Type) vtkSPtr<Type> Var = vtkSPtr<Type>::New();
+vtkSPtr<vtkRenderer> renderer;
 
 int main()
 {
     vtkSPtrNew( reader, vtkXMLPolyDataReader );
     reader->SetFileName( "../items.vtp" );
     reader->Update();
+
     vtkSPtrNew( mapper, vtkPolyDataMapper );
     mapper->SetInputData( reader->GetOutput() );
+
     vtkSPtrNew( actor, vtkActor );
     actor->SetMapper( mapper );
 
-    vtkSPtrNew( renderer, vtkRenderer );
+    vtkSPtrNew( clipPlane, vtkPlane );
+    clipPlane->SetOrigin( 0, 0, 0 );
+    clipPlane->SetNormal( 0, 0, 1 );
+
+    vtkSPtrNew( cutter, vtkCutter );
+    cutter->SetInputData( reader->GetOutput() );
+    cutter->SetCutFunction( clipPlane );
+    cutter->Update();
+
+    vtkPolyData *cutPd = cutter->GetOutput();
+    vtkSPtrNew( cutMapper, vtkPolyDataMapper );
+    cutMapper->SetInputData( cutPd );
+
+    vtkSPtrNew( cutActor, vtkActor );
+    cutActor->SetMapper( cutMapper );
+    cutActor->GetProperty()->SetColor( 1, 0, 0 );
+    cutActor->GetProperty()->SetLineWidth( 5 );
+
+    renderer = vtkSPtr<vtkRenderer>::New();
+    renderer->AddActor( cutActor );
     renderer->AddActor( actor );
     renderer->SetBackground( 0, 0, 0 );
+
+    vtkCellArray *lines = cutPd->GetLines();
+    ListsFilter listF( lines );
+    listF.SetPolyData( cutPd ); // for debug
+    listF.SetRenderer( renderer ); // for debug
+    listF.Init();
+    listF.ShowPoints();
+    listF.Update();
 
     vtkSPtrNew( renderWindow, vtkRenderWindow );
     renderWindow->AddRenderer( renderer );
